ee916c30d7d08ae29210059e4b16c1df24d156ef
David Castanheira <david.castanheira@aluno.ifsp.edu.br>
2025-02-11 15:55:11 -0300

    Desenvolvimento dos endpoints PUT e DELETE

--- controllers/users.py
+++ controllers/users.py
@@ -1,11 +1,11 @@
 import mysql.connector
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from entities.users_entities import *
 from config.database_config import connection, open_cursor, close_cursor_connection
 
 app = FastAPI()
 
-@app.get("/users", status_code=200)
+@app.get("/users", status_code=status.HTTP_302_FOUND)
 async def list_users():
     conn = None 
     cursor = None
@@ -36,7 +36,7 @@ async def list_users():
         # 5. Encerra a conexão e o cursor
         close_cursor_connection(conn, cursor)
 
-@app.get("/users/{user_id}", status_code=200)
+@app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
 async def get_user_by_id(user_id: int): 
     conn = None
     cursor = None
@@ -67,7 +67,7 @@ async def get_user_by_id(user_id: int):
     finally:
         close_cursor_connection(conn, cursor)
 
-@app.post("/users", status_code=201)
+@app.post("/users", status_code=status.HTTP_201_CREATED)
 async def create_user(user: User):
     conn = None
     cursor = None
@@ -98,3 +98,63 @@ async def create_user(user: User):
     finally:
         close_cursor_connection(conn, cursor)
 
+@app.put("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def update_user(user_id: int, user: User):
+    conn = None
+    cursor = None
+
+    try:
+        conn = connection()
+        if conn:
+            cursor = open_cursor(conn)
+
+            if cursor:
+                query_update = """
+                UPDATE User SET first_name = %s, 
+                last_name = %s,
+                gender = %s,
+                roles = %s
+                WHERE id = %s
+                """
+
+                values = (user.first_name, user.last_name, user.gender.value, ','.join([role.value for role in user.roles]), user_id)
+
+                cursor.execute(query_update, values)
+                conn.commit()
+            else:
+                raise HTTPException(status_code=500, detail="Não foi possível a criação cursor.")
+        else:
+            raise HTTPException(status_code=500, detail="Não foi possível obter uma conexão com o banco.")
+
+    except mysql.connector.Error as err:
+        print(f"Erro na conexão com o banco: {err}")
+        conn.rollback()
+
+    finally:
+        close_cursor_connection(conn, cursor)
+
+@app.delete("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def delete_users(user_id: int):
+    conn = None
+    cursor = None
+
+    try:
+        conn = connection()
+        if conn:
+            cursor = open_cursor(conn)
+
+            if cursor:
+                query_delete = "DELETE FROM User WHERE id = %s"
+                cursor.execute(query_delete, (user_id,))
+                conn.commit()
+            else:
+                raise HTTPException(status_code=500, detail="Não foi possível a criação cursor.")
+        else:
+            raise HTTPException(status_code=500, detail="Não foi possível obter uma conexão com o banco.")
+
+    except mysql.connector.Error as err:
+        print(f"Erro na conexão com o banco: {err}")
+        conn.rollback()
+
+    finally:
+        close_cursor_connection(conn, cursor)
